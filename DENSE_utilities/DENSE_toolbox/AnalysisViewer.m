e397c790a6b5dc4d680d38959426d1e06b166545
Jonathan Suever <suever@gmail.com>
2016-02-02 15:28:11 -0500

    Adds ability to export transmural strains via MAT file (Closes #4)
    DENSEanalysis was only able to compute transmural strain values when the
    user exported the results as Excel files. This limits the functionality
    because they must then be re-converted to MAT files to be processed.
    
    All transmural strain data is now written to the 'TransmuralStrainInfo'
    variable in the returned MAT file.

--- DENSE_utilities/DENSE_toolbox/AnalysisViewer.m
+++ DENSE_utilities/DENSE_toolbox/AnalysisViewer.m
@@ -1781,9 +1781,89 @@ function [file, out] = exportMatFcn(obj,startpath)
     out.DENSEInfo = DENSEInfo;
     out.SequenceInfo = SequenceInfo;
     out.StrainInfo = StrainInfo;
+    out.TransmuralStrainInfo = computeTransmuralData(obj);
     out.AnalysisInstanceUID = AnalysisInstanceUID;
 
-    set(file, '-struct', 'out')
+    save(file, '-struct', 'out')
+end
+
+function tdata = computeTransmuralData(obj)
+    % Compute transmural strain values
+    straintags  = {'RR','CC','p1','p2'};
+    strainnames = {'Err','Ecc','E1','E2'};
+
+    switch lower(obj.hdata.spl.ROIType)
+        case 'la'
+            strainnames{2} = 'Ell';
+        case 'sa'
+            straintags{end+1} = 'twist';
+            strainnames{end+1} = 'Twist';
+    end
+
+    % layers to report
+    layernames = {'mid','subepi','subendo','ave'};
+    layerids = {3, 1, 5, 1:5};
+
+    % Initialize the output structure
+    fields = cat(1,layernames, cell(size(layernames)));
+    substruct = struct(fields{:});
+
+    fields = cat(1,strainnames,repmat({substruct},size(strainnames)));
+    tdata = struct(fields{:});
+
+    [i,j]  = ndgrid(1:numel(strainnames),1:numel(layernames));
+    tags  = straintags(i);
+    lids  = layerids(j);
+    types = strainnames(i);
+    trans = layernames(j);
+
+    Nsec   = obj.straindata.Nseg;
+    Nfr    = size(obj.straindata.strain.RR,2);
+    frrng  = obj.straindata.frrng;
+    frames = frrng(1):frrng(2);
+
+    layer  = obj.straindata.fv.layerid;
+    sector = obj.straindata.fv.sectorid;
+
+    for k = numel(tags):-1:1
+        lid  = lids{k};
+        tag  = tags{k};
+        data = zeros(Nfr,Nsec);
+
+        ltf = ismember(layer,lid);
+
+        for sid = 1:Nsec
+            stf = (sector == sid);
+            tf = ltf & stf;
+
+            for fr = frames
+                data(fr,sid) = mean(obj.straindata.strain.(tag)(tf,fr));
+            end
+
+        end
+
+        tdata.(types{k}).(trans{k}) = data(frames,:);
+    end
+
+    % CURE/RURE output
+    if isequal(lower(obj.hdata.spl.ROIType),'sa')
+
+        RR = zeros(Nfr,Nsec);
+        CC = RR;
+        for sid = 1:Nsec
+            tf = (sector == sid);
+            for fr = frames
+                RR(fr,sid) = mean(obj.straindata.strain.RR(tf,fr));
+                CC(fr,sid) = mean(obj.straindata.strain.CC(tf,fr));
+            end
+        end
+
+        valcure = CURE(CC(frames,:));
+        valrure = CURE(RR(frames,:));
+
+        tdata.CURE = valcure;
+        tdata.RURE = valrure;
+    end
 end
 
 %% EXPORT EXCEL DOCUMENT
