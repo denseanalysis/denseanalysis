9908824a22db7b69e027d89420234238299c088b
Jonathan Suever <suever@gmail.com>
2016-03-01 09:04:18 -0500

    Fixes undefined variable within Mesh2D (#26)

--- DENSE_utilities/Mesh2d v24/meshfaces.m
+++ DENSE_utilities/Mesh2d v24/meshfaces.m
@@ -3,9 +3,9 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 %  MESHFACES: 2D unstructured mesh generation for polygonal geometry.
 %
 % A 2D unstructured triangular mesh is generated based on a piecewise-
-% linear geometry input. An arbitrary number of polygonal faces can be 
-% specified, and each face can contain an arbitrary number of cavities. An 
-% iterative method is implemented to optimise mesh quality. 
+% linear geometry input. An arbitrary number of polygonal faces can be
+% specified, and each face can contain an arbitrary number of cavities. An
+% iterative method is implemented to optimise mesh quality.
 %
 % If you wish to mesh a single face, use MESH2D instead!
 %
@@ -19,7 +19,7 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 %  FNUM  = Mx1 array of face numbers for each triangle in T.
 %
 % INPUTS
-%  
+%
 % Blank arguments can be passed using the empty placeholder "[]".
 %
 % NODE defines the XY co-ordinates of the geometry vertices. The vertices
@@ -33,24 +33,24 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 %  EDGE = [n1 n2; n2 n3; etc], connectivity between nodes to form
 %                              geometry edges.
 %
-% FACE defines the edges included in each geometry face. Each face is a 
+% FACE defines the edges included in each geometry face. Each face is a
 % vector of edge numbers, stored in a cell array:
 %
 %  FACE{1} = [e11,e12,etc]
 %  FACE{2} = [e21,e22,etc]
 %
-% HDATA is a structure containing user defined element size information. 
+% HDATA is a structure containing user defined element size information.
 % HDATA can include the following fields:
 %
 %  hdata.hmax  = h0;                   Max allowable global element size.
-%  hdata.edgeh = [e1,h1; e2,h2; etc];  Element size on specified geometry 
+%  hdata.edgeh = [e1,h1; e2,h2; etc];  Element size on specified geometry
 %                                      edges.
 %  hdata.fun   = 'fun' or @fun;        User defined size function.
 %  hdata.args  = {arg1, arg2, etc};    Additional arguments for HDATA.FUN.
 %
-% Calls to user specified functions must accept vectorised input of the 
+% Calls to user specified functions must accept vectorised input of the
 % form H = FUN(X,Y,ARGS{:}), where X,Y are the xy coordinates where the
-% element size will be evaluated and ARGS are optional additional arguments 
+% element size will be evaluated and ARGS are optional additional arguments
 % as passed by HDATA.ARGS.
 %
 % An automatic size function is always generated to ensure that the
@@ -60,11 +60,11 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 % OPTIONS is a structure array that allows some of the "tuning" parameters
 % used in the solver to be modified:
 %
-%   options.mlim   : The convergence tolerance. The maximum percentage 
-%                    change in edge length per iteration must be less than 
-%                    MLIM { 0.02, 2.0% }. 
+%   options.mlim   : The convergence tolerance. The maximum percentage
+%                    change in edge length per iteration must be less than
+%                    MLIM { 0.02, 2.0% }.
 %   options.maxit  : The maximum allowable number of iterations { 20 }.
-%   options.dhmax  : The maximum allowable (relative) gradient in the size 
+%   options.dhmax  : The maximum allowable (relative) gradient in the size
 %                    function { 0.3, 30.0% }.
 %   options.output : Displays the mesh and the mesh statistics upon
 %                    completion { TRUE }.
@@ -76,7 +76,7 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 %
 % See also MESH2D, REFINE, SMOOTHMESH, DELAUNAYN
 
-% STATS is an undocumented output used in debugging. Returns the algorithm 
+% STATS is an undocumented output used in debugging. Returns the algorithm
 % statistics usually printed to screen as a structure.
 
 %   Darren Engwirda : 2005-09
@@ -89,7 +89,7 @@ function [p,t,fnum,stats] = meshfaces(node,edge,face,hdata,options)
 ts = cputime;
 
 % Catch any initalisation errors
-try                                                   
+try
 
    % I/O error checks
    if (nargin<5)
@@ -115,17 +115,15 @@ try
 
    % Get user options
    options = getoptions(options);
-   
+
    % Check geometry and attempt to repair bad geometry
    if options.output
       fprintf('Checking Geometry\n');
    end
-   [node,edge,face,hdata] = checkgeometry(node,edge,face,hdata);      
-   
-catch
-   % Close waitbar on error
-   close(wbar);
-   rethrow(lasterror);
+   [node,edge,face,hdata] = checkgeometry(node,edge,face,hdata);
+
+catch ME
+   rethrow(ME);
 end
 
 % Quadtree decomposition
@@ -142,7 +140,7 @@ pbnd = boundarynodes(qtree.p,qtree.t,qtree.h,node,edge,options.output);
 % Mesh each face separately
 p = []; t = []; fnum = [];
 for k = 1:length(face)
-   
+
    % Mesh kth polygon
    [pnew,tnew] = meshpoly(node,edge(face{k},:),qtree,pbnd,options);
 
@@ -150,11 +148,11 @@ for k = 1:length(face)
    t = [t; tnew+size(p,1)];
    p = [p; pnew];
    fnum = [fnum; k*ones(size(tnew,1),1)];
-   
+
 end
 
 % Ensure consistent, CCW ordered triangulation
-[p,t,fnum,fnum] = fixmesh(p,t,[],fnum);   
+[p,t,fnum,fnum] = fixmesh(p,t,[],fnum);
 
 % Element quality
 q = quality(p,t);
@@ -162,7 +160,7 @@ q = quality(p,t);
 % Method statistics
 stats = struct('Time',cputime-ts,'Triangles',size(t,1), ...
                   'Nodes',size(p,1),'Mean_quality',mean(q),'Min_quality',min(q));
-               
+
 if options.output
    figure('Name','Mesh')
    plot(p(:,1),p(:,2),'b.','markersize',1)
@@ -191,12 +189,12 @@ end      % meshfaces()
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function p = boundarynodes(ph,th,hh,node,edge,output)
 
-% Discretise the geometry based on the edge size requirements interpolated 
+% Discretise the geometry based on the edge size requirements interpolated
 % from the background mesh.
 
 p = node;
 e = edge;
-i = tsearch(ph(:,1),ph(:,2),th,p(:,1),p(:,2));               
+i = tsearch(ph(:,1),ph(:,2),th,p(:,1),p(:,2));
 h = tinterp(ph,th,hh,p,i);
 
 if output
@@ -204,11 +202,11 @@ if output
 end
 iter = 1;
 while true
-   
+
    % Edge length
    dxy = p(e(:,2),:)-p(e(:,1),:);
    L = sqrt(sum(dxy.^2,2));
-   % Size function on edges                                           
+   % Size function on edges
    he = 0.5*(h(e(:,1))+h(e(:,2)));
    % Split long edges
    ratio = L./he;
@@ -224,7 +222,7 @@ while true
       e = [e; n3,n2];
       p = [p; pm];
       % Size function at new nodes
-      i = mytsearch(ph(:,1),ph(:,2),th,pm(:,1),pm(:,2));               
+      i = mytsearch(ph(:,1),ph(:,2),th,pm(:,1),pm(:,2));
       h = [h; tinterp(ph,th,hh,pm,i)];
    else
       break
@@ -244,14 +242,14 @@ del = 0.0;
 tol = 0.02;
 maxit = 50;
 i = zeros(size(p,1),1);
-for iter = 1:maxit 
+for iter = 1:maxit
 
    delold = del;
-   
+
    % Spring based smoothing
    F = he./L-1.0;
    F = S*(dxy.*[F,F]);
-   F(1:size(node,1),:) = 0.0;   
+   F(1:size(node,1),:) = 0.0;
    p = p+0.2*F;
 
    % Convergence
@@ -266,14 +264,14 @@ for iter = 1:maxit
       end
    end
    L = Lnew;
-   
+
    if (del>delold)
       % Interpolate required size at new P
       i = mytsearch(ph(:,1),ph(:,2),th,p(:,1),p(:,2),i);
       h = tinterp(ph,th,hh,p,i);
       he = 0.5*(h(e(:,1))+h(e(:,2)));
    end
-   
+
 end
 
 end
