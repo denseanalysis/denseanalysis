05d3361616383c7291f4ac90b4c595118cb858e8
Eric Carruth <eric.d.carruth@gmail.com>
2018-12-03 09:59:17 -0500

    Fixes LVLongAxis filled chamber bug

--- DENSE_utilities/DENSE_toolbox/maskGeneral.m
+++ DENSE_utilities/DENSE_toolbox/maskGeneral.m
@@ -1,6 +1,13 @@
 function tf = maskGeneral(X, Y, C)
-    tf = false(size(X));
-    for n = 1:numel(C)
-        tf = tf | inpolygon(X, Y, C{n}(:,1), C{n}(:,2));
-    end
+	% % Previous code before 11/30/2018 resulted in a filled chamber for
+	% % LVLongAxis type contours
+ %    tf = false(size(X));
+ %    for n = 1:numel(C)
+ %        tf = tf | inpolygon(X, Y, C{n}(:,1), C{n}(:,2));
+ %    end
+ 	% New version as of 11/30/2018 - all 8 contour types
+ 	% (including dual ventricle types from the dense3D_plugin)
+ 	% work correctly after this update - EDC
+ 	C = cat(1,C{:});
+ 	tf = inpolygon(X, Y, C(:,1), C(:,2));
 end
